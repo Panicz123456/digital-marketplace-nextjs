f0963cf6c94e845d331d008252a432c56ae23bf3
patryk <paniczgg@gmail.com>
2025-01-07 20:21:47 +0100

    add sell form

---
+++ app/components/SelectCategory.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Card, CardHeader } from "@/components/ui/card";
+import { categoryItems } from "../lib/categroyItems";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+
+export const SelectCategory = () => {
+  const [selectCategory, setSelectedCategory] = useState<string | null>(null);
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+      {categoryItems.map((item) => (
+        <div key={item.id} className="cursor-pointer">
+          <Card
+            className={
+              selectCategory === item.name
+                ? "border-primary border-2"
+                : "border-2 border-primary/10"
+            }
+            onClick={() => setSelectedCategory(item.name)}>
+            <CardHeader>
+              {item.image} <h3 className="font-medium">{item.title}</h3>
+            </CardHeader>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
+};
---
+++ app/sell/page.tsx
@@ -0,0 +1,73 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+import { SelectCategory } from "../components/SelectCategory";
+import { TipTapEditor } from "../components/Editor";
+import { UploadButton, UploadDropzone } from "../lib/uploadthing";
+import { Button } from "@/components/ui/button";
+
+const PageSell = () => {
+  return (
+    <section className="max-w-7xl mx-auto px-4 md:px-8 mb-14">
+      <Card>
+        <form>
+          <CardHeader>
+            <CardTitle>Sell your product with ease</CardTitle>
+            <CardDescription>
+              Please describe your product here in details so that it can be
+              sold
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-y-10">
+            <div className="flex flex-col gap-y-2">
+              <Label>Name</Label>
+              <Input type="text" placeholder="Name of your product" />
+            </div>
+            <div className="flex flex-col gap-y-2">
+              <Label>Category</Label>
+              <SelectCategory />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Price</Label>
+              <Input placeholder="28$" type="number" />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Small Summary</Label>
+              <Textarea placeholder="Please describe your product shortly right here..." />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Description</Label>
+              <TipTapEditor />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <Label>Product Images</Label>
+              <UploadDropzone endpoint="imageUploader" />
+            </div>
+            <div className="flex flex-col gap-y-2">
+              <Label>Product File</Label>
+              <UploadDropzone endpoint="productFileUpload" />
+            </div>
+          </CardContent>
+          <CardFooter className="mt-5">
+            <Button>Submit</Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </section>
+  );
+};
+
+export default PageSell;
